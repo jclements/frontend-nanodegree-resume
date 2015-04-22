d6bedd176e04a8f33c333505c93279f8f91438fc
Jeremy <jeremy@home>
2015-04-21 22:57:58 -0600

    Lesson 1-5

---
+++ js/resumeBuilder.js
@@ -0,0 +1,5 @@
+var formattedName = HTMLheaderName.replace("%data%","Jeremy Clements");
+var formattedRole = HTMLheaderRole.replace("%data%","Web Developer");
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
