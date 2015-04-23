73a5dbba0211a09d74a2928b41236dfb205df6ce
Jeremy <jeremy@home>
2015-04-22 21:14:04 -0600

    Error2

---
+++ js/resumeBuilder.js
@@ -0,0 +1,8 @@
+<<<<<<< HEAD
+=======
+var formattedName = HTMLheaderName.replace("%data%","Jeremy Clements");
+var formattedRole = HTMLheaderRole.replace("%data%","Web Developer");
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+>>>>>>> parent of 7a804f7... Error?
