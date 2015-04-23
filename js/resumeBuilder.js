7a804f7e6e82cf38b68c318c0d82ca7121844893
Jeremy <jeremy@home>
2015-04-22 21:12:01 -0600

    Error?

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,5 @@
 var formattedName = HTMLheaderName.replace("%data%","Jeremy Clements");
 var formattedRole = HTMLheaderRole.replace("%data%","Web Developer");
 
-$("#header").append(formattedName);
-$("#header").append(formattedRole);
+$("#header").prepend(formattedRole);
+
