bd795128beab5b7182e374a4086a4762e9dbd59c
Jeremy <jeremy@home>
2015-04-22 21:15:26 -0600

    w

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,5 +4,9 @@ var formattedName = HTMLheaderName.replace("%data%","Jeremy Clements");
 var formattedRole = HTMLheaderRole.replace("%data%","Web Developer");
 
 $("#header").append(formattedName);
+<<<<<<< HEAD
+$("#header").append(formattedRole);
+>>>>>>> parent of 7a804f7... Error?
+=======
 $("#header").append(formattedRole);
 >>>>>>> parent of 7a804f7... Error?
