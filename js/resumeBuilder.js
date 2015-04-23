dc67092fd10d3bbb9b3bb212f1e8c3cc8f229e24
Jeremy <jeremy@home>
2015-04-22 21:12:29 -0600

    Revert "Lesson 1-5"
    This reverts commit d6bedd176e04a8f33c333505c93279f8f91438fc.

--- js/resumeBuilder.js
+++
@@ -1,5 +0,0 @@
-var formattedName = HTMLheaderName.replace("%data%","Jeremy Clements");
-var formattedRole = HTMLheaderRole.replace("%data%","Web Developer");
-
-$("#header").append(formattedName);
-$("#header").append(formattedRole);
